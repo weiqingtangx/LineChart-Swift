e611aa7be5216230c2b602f2bd04293648b84635
weiqing.twq <weiqing.twq@antgroup.com>
2022-06-22 08:42:40 +0800

    feat:update function name

--- LineChart-Swift/ViewController.swift
+++ LineChart-Swift/ViewController.swift
@@ -18,11 +18,11 @@ class ViewController: UIViewController {
         self.title = "折线图";
         
         //step1 初始化一个view来承载图表的显示
-        self.canvasView = F2CanvasView.canvas(withFrame: CGRect(x:0, y: 100, width: self.view.frame.width, height: 200))
+        self.canvasView = F2CanvasView.canvas(CGRect(x:0, y: 100, width: self.view.frame.width, height: 200))
         self.view.addSubview(self.canvasView!)
         
         //step2 创建一个chart对象准备绘制图表
-        self.chart = F2Chart.init(self.canvasView!.bounds.size, withName: "LineChart-Swift")
+        self.chart = F2Chart.init(self.canvasView!.bounds.size, name: "LineChart-Swift")
         
         //step3 把chart和canvasView关联起来，因为chart渲染好后需要显示在view上面
         self.chart!.canvas()(self.canvasView!)
@@ -37,7 +37,8 @@ class ViewController: UIViewController {
         self.chart!.source()(jsonData)
         
         //step5 在chart上画一条线，x轴数据的映射是genre字段，y轴数据的映射是sold字段
-        self.chart!.interval()().position()("genre*sold")
+        //self.chart!.interval()().position()("genre*sold")
+        self.chart!.line()().position()("genre*sold")
         
         //step6 渲染并显示在view上
         self.chart!.render()();
